4b083799ee2a553864de2dcbf70133b71e9d1a64
Salvatore <carlos0scampo@gmail.com>
2025-02-27 08:02:33 -0500

    first commit

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -11,12 +11,6 @@ const users = [
 
 const customers = [
   {
-    id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
-    name: 'Evil Rabbit',
-    email: 'evil@rabbit.com',
-    image_url: '/customers/evil-rabbit.png',
-  },
-  {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
     name: 'Delba de Oliveira',
     email: 'delba@oliveira.com',
@@ -29,19 +23,49 @@ const customers = [
     image_url: '/customers/lee-robinson.png',
   },
   {
+    id: '3958dc9e-737f-4377-85e9-fec4b6a6442a',
+    name: 'Hector Simpson',
+    email: 'hector@simpson.com',
+    image_url: '/customers/hector-simpson.png',
+  },
+  {
+    id: '50ca3e18-62cd-11ee-8c99-0242ac120002',
+    name: 'Steven Tey',
+    email: 'steven@tey.com',
+    image_url: '/customers/steven-tey.png',
+  },
+  {
+    id: '3958dc9e-787f-4377-85e9-fec4b6a6442a',
+    name: 'Steph Dietz',
+    email: 'steph@dietz.com',
+    image_url: '/customers/steph-dietz.png',
+  },
+  {
     id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
     name: 'Michael Novotny',
     email: 'michael@novotny.com',
     image_url: '/customers/michael-novotny.png',
   },
   {
-    id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
+    id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
+    name: 'Evil Rabbit',
+    email: 'evil@rabbit.com',
+    image_url: '/customers/evil-rabbit.png',
+  },
+  {
+    id: '126eed9c-c90c-4ef6-a4a8-fcf7408d3c66',
+    name: 'Emil Kowalski',
+    email: 'emil@kowalski.com',
+    image_url: '/customers/emil-kowalski.png',
+  },
+  {
+    id: 'cc27c14a-0acf-4f4a-a6c9-d45682c144b9',
     name: 'Amy Burns',
     email: 'amy@burns.com',
     image_url: '/customers/amy-burns.png',
   },
   {
-    id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
+    id: '13d07535-c59e-4157-a011-f8d2ef4e0cbb',
     name: 'Balazs Orban',
     email: 'balazs@orban.com',
     image_url: '/customers/balazs-orban.png',
@@ -80,13 +104,13 @@ const invoices = [
     date: '2023-08-05',
   },
   {
-    customer_id: customers[2].id,
+    customer_id: customers[7].id,
     amount: 54246,
     status: 'pending',
     date: '2023-07-16',
   },
   {
-    customer_id: customers[0].id,
+    customer_id: customers[6].id,
     amount: 666,
     status: 'pending',
     date: '2023-06-27',
@@ -123,6 +147,18 @@ const invoices = [
   },
   {
     customer_id: customers[2].id,
+    amount: 8945,
+    status: 'paid',
+    date: '2023-06-18',
+  },
+  {
+    customer_id: customers[0].id,
+    amount: 8945,
+    status: 'paid',
+    date: '2023-10-04',
+  },
+  {
+    customer_id: customers[2].id,
     amount: 1000,
     status: 'paid',
     date: '2022-06-05',
@@ -144,4 +180,9 @@ const revenue = [
   { month: 'Dec', revenue: 4800 },
 ];
 
-export { users, customers, invoices, revenue };
+module.exports = {
+  users,
+  customers,
+  invoices,
+  revenue,
+};
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,10 +1,8 @@
 import { Inter, Lusitana } from 'next/font/google';
 
-// Fuente principal
 export const inter = Inter({ subsets: ['latin'] });
 
-// Fuente secundaria
 export const lusitana = Lusitana({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-});
+    weight: ['400', '700'],
+    subsets: ['latin']
+})
